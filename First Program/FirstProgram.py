0b81c253499f612565d99a23706d96bde2b08409
Raspi Kidd <raspi.kidd@gmail.com>
2015-04-13 17:55:12 +0100

    FirstProgram

---
+++ First Program/FirstProgram.py
@@ -0,0 +1,13 @@
+#! /usr/bin python
+
+# My first python Program
+
+
+# This will print out in the terminal
+print "Hello. I am a Python program"
+
+# This line will take input from the user and store it as a variable called name to use later
+name = raw_input ("What is your name? ")
+
+# This line will print out "Hello there" followed by what is stored in the name variable
+print "Hello there, " + name + "!"
